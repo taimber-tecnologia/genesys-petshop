2ec8ff7cec68e98a674bfedc6f237d5264e048b4
salomaotech <salomao@taimber.com>
2023-02-04 19:00:08 -0300

    Kernel unico

--- src/main/java/br/com/salomaotech/sistema/patterns/Command.java
+++
@@ -1,7 +0,0 @@
-package br.com.salomaotech.sistema.patterns;
-
-public interface Command {
-
-    public void executar(Object arg);
-
-}
--- src/main/java/br/com/salomaotech/sistema/patterns/Modelo.java
+++
@@ -1,9 +0,0 @@
-package br.com.salomaotech.sistema.patterns;
-
-public interface Modelo {
-
-    public abstract void setId(long id);
-
-    public abstract long getId();
-
-}
